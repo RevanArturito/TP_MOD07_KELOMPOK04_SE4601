6c35f85dbff0b39f415d0a3d79cfe748241e9df4
Rifqi Alghifari <137023042+DomiQQ99@users.noreply.github.com>
2024-04-01 00:58:46 +0700

    menambahkan class <DataMahasiswa_1302223028>

--- tpmod07_kelompok_04/tpmod07_kelompok_04/Program.cs
+++ tpmod07_kelompok_04/tpmod07_kelompok_04/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmod07_kelompok_04;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa_1302223028 mahasiswa = new DataMahasiswa_1302223028();
+        mahasiswa.ReadJSON();
+    }
+}
