48273eb667a8ce65057fd33efa220f45ffe2c4b5
Faisal Ramadhan <rdmfaisalrj@student.telkomuniversity.ac.id>
2024-04-01 00:29:38 +0700

    menambahkan json dan class data mahasiswa by xall

---
+++ tpmod07_kelompok_04/tpmod07_kelompok_04/DataMahasiswa_1302220093.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using System.Xml.Linq;
+public class DataMahasiswa_1302220093
+{
+    public Name nama { set; get; }
+    public int nim { set; get; }
+    public string fakultas { set; get; }
+
+    public class Name
+    {
+        public string depan { set; get; }
+        public string belakang { set; get; }
+    }
+
+    public void ReadJSON()
+    {
+        string filePath = "C:\\Programing\\C#\\mod07\\TP_MOD07_KELOMPOK04_SE4601\\tpmod07_kelompok_04\\tpmod07_kelompok_04";
+        string jsonString = File.ReadAllText(filePath);
+
+        DataMahasiswa_1302220093 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa_1302220093>(jsonString);
+        Console.WriteLine($"Nama : {mahasiswa.nama.depan} {mahasiswa.nama.belakang}");
+        Console.WriteLine($"NIM : {mahasiswa.nim}");
+        Console.WriteLine($"Fakultas : {mahasiswa.fakultas}");
+    }
+}
--- tpmod07_kelompok_04/tpmod07_kelompok_04/Program.cs
+++ tpmod07_kelompok_04/tpmod07_kelompok_04/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Program
+{
+    public static void main(string[] args)
+    {
+        DataMahasiswa_1302220093 mahasiswa = new DataMahasiswa_1302220093();
+        mahasiswa.ReadJSON();
+    }
+}
