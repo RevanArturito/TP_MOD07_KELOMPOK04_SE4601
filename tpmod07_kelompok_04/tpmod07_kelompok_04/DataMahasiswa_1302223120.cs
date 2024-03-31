242d5d4783e13935aa8c99a6f4668c575fb2357b
Faishal Harits Al Fajri <faishalharits@student.telkomuniversity.ac.id>
2024-04-01 01:14:38 +0700

    Menambahkan harits_DataMahasiswa

---
+++ tpmod07_kelompok_04/tpmod07_kelompok_04/DataMahasiswa_1302223120.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace tpmod07_kelompok_04
+{
+
+    public class DataMahasiswa_1302223120
+    {
+        public class Mahasiswa_1302223120() 
+        {
+            public Name nama { set; get; }
+            public int nim { set; get; }
+            public string fakultas { set; get; }
+        }
+
+        public class Name
+        {
+            public string depan { set; get; }
+            public string belakang { set; get; }
+        }
+        public void ReadJSON()
+        {
+            string filePath = "D:\\codingan\\TP_MOD_07_1302223120_Faishal Harits Al Fajri_Kelompok_4\\TP_MOD07_KELOMPOK04_SE4601\\tpmod07_kelompok_04\\tp7_1_1302223120.json";
+            string jsonString = File.ReadAllText(filePath);
+
+            Mahasiswa_1302223120 mahasiswa = JsonSerializer.Deserialize<Mahasiswa_1302223120>(jsonString);
+            Console.WriteLine($"Nama : {mahasiswa.nama.depan} {mahasiswa.nama.belakang}");
+            Console.WriteLine($"NIM : {mahasiswa.nim}");
+            Console.WriteLine($"Fakultas : {mahasiswa.fakultas}");
+        }
+    }
+}
