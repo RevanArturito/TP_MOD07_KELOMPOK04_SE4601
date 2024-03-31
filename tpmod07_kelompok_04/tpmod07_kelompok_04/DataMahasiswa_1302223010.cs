955d321d26faf31840aafbe5ee140128ebd0f054
shad kinx <rivapermana763@gmail,com>
2024-04-01 00:52:50 +0700

    menambahkan class DataMahasiswa_1302223010

---
+++ tpmod07_kelompok_04/tpmod07_kelompok_04/DataMahasiswa_1302223010.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmod07_kelompok_04
+{
+    internal class DataMahasiswa_1302223010
+    {
+        public class Mahasiswa_1302223010()
+        {
+            public Name nama { set; get; }
+            public int nim { set; get; }
+            public string fakultas { set; get; }
+        }
+
+        public class Name
+        {
+            public string depan { set; get; }
+            public string belakang { set; get; }
+        }
+
+        public void ReadJSON()
+        {
+            string filePath = "D:\\VS\\tpmod7\\TP_MOD07_KELOMPOK04_SE4601\\tpmod07_kelompok_04\\tpmod07_kelompok_04\\tp7_1_1302223010.json";
+            string jsonString = File.ReadAllText(filePath);
+
+            Mahasiswa_1302223010 mahasiswa = JsonSerializer.Deserialize<Mahasiswa_1302223010>(jsonString);
+            Console.WriteLine($"Nama : {mahasiswa.nama.depan} {mahasiswa.nama.belakang}");
+            Console.WriteLine($"NIM : {mahasiswa.nim}");
+            Console.WriteLine($"Fakultas : {mahasiswa.fakultas}");
+        }
+    }
+}
--- tpmod07_kelompok_04/tpmod07_kelompok_04/Program.cs
+++ tpmod07_kelompok_04/tpmod07_kelompok_04/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmod07_kelompok_04;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa_1302223010 mahasiswa = new DataMahasiswa_1302223010();
+        mahasiswa.ReadJSON();
+    }
+}
